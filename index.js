22cf0cdb92dbc98df988eb63a3e766c5d430fc78
Ottochy <102737499+Ottochy@users.noreply.github.com>
2022-05-05 08:59:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//Exercícios - Otavio Martins da Silva
+//1-a
+
+console.log(10+20*30);
+
+//1-b
+
+console.log(4**2/30);
+
+//1-c
+
+console.log((9**4+2)*6-1);
+
+//2-a
+
+console.log(10%3*10**1+1-10*4/2);
+
+//3
+
+console.log('Otavio');
+
+//4
+
+console.log(2*3*3*5);
+
+//5
+
+const a = 1;
+const b = 6;
+const c = 3;
+console.log(a+b+c);
+
+//6
+
+console.log(1800*2)
